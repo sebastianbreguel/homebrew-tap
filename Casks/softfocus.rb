1df8584c68b5e02e29b1cbcdd679d7d144b5369a
Sebastian Breguel <breguelsebastian@gmail.com>
2026-06-23 22:07:09 -0400

    Bump softfocus to 1.1.0

--- Casks/softfocus.rb
+++ Casks/softfocus.rb
@@ -1,6 +1,6 @@
 cask "softfocus" do
-  version "1.0.2"
-  sha256 "75335bba6d7034b74656faf1a447c18389808d478905371513f3ba0dd66c81ef"
+  version "1.1.0"
+  sha256 "dc714ebfc41f67e0115098130d185e5d1fb114af0ed6e69d0766a19ff44c79bb"
 
   url "https://github.com/sebastianbreguel/softfocus/releases/download/v#{version}/SoftFocus-#{version}.zip"
   name "SoftFocus"
